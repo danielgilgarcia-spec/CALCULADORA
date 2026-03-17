1062fd78c08690485c10fc5be9cc9554b469ca45
Dani <daniel.gil.garcia@estudiantat.upc.edu>
2026-03-17 18:36:35 +0100

    ok

--- feature pantalla/test_pantalla.py
+++ feature pantalla/test_pantalla.py
@@ -1,7 +1,8 @@
-@import sys
+import sys
 from PySide6 import QtWidgets
 # Importamos tu clase Vista y el Modelo (aunque el Mock no use el modelo real)
 from PantallaCalculadora import Pantalla
+
 
 
 class MockPresenter:
@@ -14,11 +15,41 @@ class MockPresenter:
         self.view = view
         # Nos conectamos a la señal de la vista según el contrato [6]
         self.view.btnsuma.connect(self.al_recibir_suma)
-        print("MOCK: Presenter conectado a la señal 'btnsuma' de la Vista.")
+        self.view.btnresta.connect(self.al_recibir_resta)
+        self.view.btnmultiplicacion.connect(self.al_recibir_multiplicacion)
+        self.view.btndivision.connect(self.al_recibir_division)
+        print("MOCK: Presenter conectado a las señales de la Vista.")
 
     def al_recibir_suma(self):
         """Este método se ejecuta cuando la Vista emite la señal btnsuma"""
         print("\n[OK] MOCK: El Presenter ha recibido la señal de SUMA desde la interfaz.")
+
+        # Para probar el flujo completo, el Mock puede "ordenar" a la vista
+        # que muestre un valor ficticio [7]
+        print("MOCK: Ordenando a la vista mostrar el resultado de prueba '99.999'...")
+        self.view.salida(99.999)
+    
+    def al_recibir_resta(self):
+        """Este método se ejecuta cuando la Vista emite la señal btnresta"""
+        print("\n[OK] MOCK: El Presenter ha recibido la señal de RESTA desde la interfaz.")
+
+        # Para probar el flujo completo, el Mock puede "ordenar" a la vista
+        # que muestre un valor ficticio [7]
+        print("MOCK: Ordenando a la vista mostrar el resultado de prueba '99.999'...")
+        self.view.salida(99.999)
+
+    def al_recibir_multiplicacion(self):
+        """Este método se ejecuta cuando la Vista emite la señal btnmultiplicacion"""
+        print("\n[OK] MOCK: El Presenter ha recibido la señal de MULTIPLICACIÓN desde la interfaz.")
+
+        # Para probar el flujo completo, el Mock puede "ordenar" a la vista
+        # que muestre un valor ficticio [7]
+        print("MOCK: Ordenando a la vista mostrar el resultado de prueba '99.999'...")
+        self.view.salida(99.999)
+
+    def al_recibir_division(self):
+        """Este método se ejecuta cuando la Vista emite la señal btndivision"""
+        print("\n[OK] MOCK: El Presenter ha recibido la señal de DIVISIÓN desde la interfaz.")
 
         # Para probar el flujo completo, el Mock puede "ordenar" a la vista
         # que muestre un valor ficticio [7]
